ae468f44dd5dbd1ae09e746d2025bf290d780ee1
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2024-02-29 20:59:04 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -243,8 +243,14 @@ def show_possible_matches(my_word):
              that has already been revealed.
 
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+        matches = ""
+    for word in wordlist:
+        if match_with_gaps(my_word, word):
+            matches += word + " "
+    if matches:     
+       print(matches)
+    else: 
+        print("No matches found")
 
 
 
